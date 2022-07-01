4d2e468db14c04fcf3c9f4245c5b34bc5b430901
Mikko Larinen <69403498+mikkolar@users.noreply.github.com>
2022-07-01 08:15:26 +0300

    Added DataTypes

--- ConferenceTracker/Entities/Speaker.cs
+++ ConferenceTracker/Entities/Speaker.cs
@@ -10,15 +10,20 @@ namespace ConferenceTracker.Entities
         public int Id { get; set; }
         [Required]
         [Display(Name = "First name")]
+        [DataType(DataType.Text)]
         public string FirstName { get; set; }
         [Required]
         [Display(Name = "Last name")]
+        [DataType(DataType.Text)]
         public string LastName { get; set; }
         [Required]
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
         [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress)]
         public string EmailAddress { get; set; }
         [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
         public string PhoneNumber { get; set; }
         public bool IsStaff { get; set; }
     }
